0f25248c60fb8774db0ef82bd2e2c40518ff240d
Jaishree Bala <jaishree1bala@gmail.com>
2020-11-16 02:54:33 -0500

    Home page: svg animation, responsiveness

--- Javascript/app.js
+++ Javascript/app.js
@@ -2,10 +2,16 @@
 const hamburger = document.querySelector(".hamburger")
 const menuItems = document.querySelector(".menuItems")
 const listItems = document.querySelectorAll(".menuItems a");
+const swipeText = document.querySelector(".text");
+let swipeTextList = ["Full Stack Web developer", "Game Development Enthusiast", "Data Science Enthusiast", "UI/UX Designer"];
+let countText = 1;
+let delayCounter = 0;
 // Functions
 
 hamburger.addEventListener("click", () => {
-    menuItems.classList.toggle('toggleMenu')
+    menuItems.classList.toggle('toggleMenu');
+    document.body.classList.toggle("hidden");
+
     listItems.forEach(listItem => {
         listItem.classList.toggle('fade')
     })
@@ -13,13 +19,32 @@ hamburger.addEventListener("click", () => {
 listItems.forEach(listItem => {
     listItem.addEventListener("click", () => {
         menuItems.classList.toggle('toggleMenu')
+        document.body.classList.toggle("hidden");
+
         listItems.forEach(listItemTwo => {
             console.log(listItemTwo)
             listItemTwo.classList.remove("selected")
             listItemTwo.classList.toggle('fade')
         })
         listItem.classList.add("selected");
-
     })
 })
 
+
+function typewriter() {
+    console.log(swipeTextList[countText])
+    countText++;
+    delayCounter++;
+    swipeText.style.animation = `swipeAnimation 1s ease ${3 * delayCounter}s`;
+    setTimeout(() => {
+        swipeText.innerText = swipeTextList[countText];
+    }, 500)
+    console.log(swipeTextList[countText])
+    if (countText == 4) {
+        countText = 0;
+    }
+    setTimeout(typewriter, 3000)
+
+}
+
+setTimeout(typewriter, 3000)
