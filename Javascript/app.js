b16f5f3386644f7f691b8b7cb81ac6452ec84e02
Jaishree Bala <jaishree1bala@gmail.com>
2020-11-19 15:00:13 -0500

    Interactive cursor, updated resume

--- Javascript/app.js
+++ Javascript/app.js
@@ -3,9 +3,14 @@ const hamburger = document.querySelector(".hamburger")
 const menuItems = document.querySelector(".menuItems")
 const listItems = document.querySelectorAll(".menuItems a");
 const swipeText = document.querySelector(".text");
+const logo = document.querySelector(".logo img");
+const burger = document.querySelector(".hamburger");
 let swipeTextList = ["Full Stack Web developer", "Game Development Enthusiast", "Data Science Enthusiast", "UI/UX Designer"];
 let countText = 1;
 let delayCounter = 0;
+let mouse = document.querySelector(".cursor");
+let interactiveText = mouse.querySelector("span");
+
 // Functions
 
 hamburger.addEventListener("click", () => {
@@ -22,7 +27,6 @@ listItems.forEach(listItem => {
         document.body.classList.toggle("hidden");
 
         listItems.forEach(listItemTwo => {
-            console.log(listItemTwo)
             listItemTwo.classList.remove("selected")
             listItemTwo.classList.toggle('fade')
         })
@@ -32,14 +36,12 @@ listItems.forEach(listItem => {
 
 
 function typewriter() {
-    console.log(swipeTextList[countText])
     countText++;
     delayCounter++;
     swipeText.style.animation = `swipeAnimation 1s ease ${3 * delayCounter}s`;
     setTimeout(() => {
         swipeText.innerText = swipeTextList[countText];
     }, 500)
-    console.log(swipeTextList[countText])
     if (countText == 4) {
         countText = 0;
     }
@@ -48,3 +50,22 @@ function typewriter() {
 }
 
 setTimeout(typewriter, 3000)
+
+
+function cursor(e) {
+    mouse.style.top = e.pageY + "px";
+    mouse.style.left = e.pageX + "px";
+}
+function interactiveMouse(e) {
+    const item = e.target;
+    console.log(item.parentNode.classList)
+    if (item.id === "logo" || item.id === "burger" || item.classList.contains('button') || item.parentNode.classList.contains("links-area") || item.parentNode.classList.contains("menuLinks")) {
+        mouse.classList.add("interactive-purple")
+        interactiveText.innerHTML = item.innerText || "tap";
+    } else {
+        mouse.classList.remove("interactive-purple");
+        interactiveText.innerHTML = "";
+    }
+}
+window.addEventListener("mousemove", cursor);
+window.addEventListener("mouseover", interactiveMouse);
